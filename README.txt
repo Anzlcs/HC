429ce0b2985b1708924d91c8aed2dd6f3cf9bc51
Anzlcs <coc16092011@gmail.com>
2025-07-19 16:47:39 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+This project is open source, but all rights belong to @anzlcs.
+If you plan to modify or develop the project further, please contact me on Telegram: @anzlc.
+Removing or altering the creator’s name is strictly prohibited.
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html", "./*.js"],
+  darkMode: 'class',
+  safelist: [
+    "text-red-600",
+    "text-yellow-500",
+    "text-green-600",
+    "text-blue-600",
+    "bg-red-100",
+    "bg-yellow-100",
+    "bg-green-100",
+    "bg-blue-100",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
